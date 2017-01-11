b289cdde19f551aaf2d09e5fbc626c841b6df842
Romain-Richard <melrom64@gmail.com>
2017-01-11 09:49:09 +0100

    Test CKEditor + Detroyed Turbolinks

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -5,7 +5,8 @@ Ckeditor.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'ckeditor/orm/active_record'
-
+  config.assets_languages = ['en', 'fr']
+  config.assets_plugins = ['image', 'smiley']
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
